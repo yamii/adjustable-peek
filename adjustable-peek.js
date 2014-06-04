92128910bbda03ff6fa9695ccff217d79b03994b
Miriam Mae Iglesia <miriamiglesia@gmail.com>
2014-06-04 16:07:57 +0800

    Tested and added resize functionality.

--- adjustable-peek.js
+++ adjustable-peek.js
@@ -9,36 +9,102 @@
 ( function ( $ ) {
 	'use strict';
 
+	var MAX_PEEK = 50;
+	var MIN_PEEK = 40;
+	var MAX_RANGE = 100;
+	var MIN_RANGE = 1;
 
 	var AdjustablePeek = function ( element, options ) {
 
-		this.settings        = $.extend( {}, $.fn.adjustablePeek.defaults, options );
-		this.element         = $( element );
-		this.itemElem        = $( element ).find( '.' + this.settings.itemClass + ':first' );
+		this.settings      = $.extend( {}, $.fn.adjustablePeek.defaults, options );
+		this.element       = element;
+		this.itemElem      = $( this.element ).find( '.' + this.settings.itemClass + ':first' );
+		this.itemElemWidth = this.itemElem.outerWidth();
 
+		this.initPeekSettings();
+		this.start();
 
-		var nContainerWidth = this.element.width();
-		var nItemWidth      = this.itemElem.outerWidth();
-		var nPartialElem    = nContainerWidth % nItemWidth;
+		this.handleResize();
+	};
+
+	AdjustablePeek.prototype.start = function () {
+
+		this.clearInlineStyles();
+
+		var eContainer       = $( this.element ).closest( '#' + this.settings.containerId );
+
+		if( this.settings.containerId === '' ) {
+			eContainer = $( this.element );
+		}
+
+		var nContainerWidth = eContainer.outerWidth();
+		var nItemWidth      = this.itemElem.outerWidth( true );
+		var nPartialElem    = ( ( nContainerWidth  / nItemWidth ) % 1 )  +
+													( nContainerWidth % nItemWidth );
+
 		var nActive         = Math.floor( nContainerWidth / nItemWidth );
 
 		this.nPeek          = ( nPartialElem > 0 ) ?
-										 (  ( nPartialElem / nItemWidth ) * 100 ) : 0;
+													(  ( nPartialElem / nItemWidth ) * 100 ) : 0;
 
 		if( ! this.adjustIfBelowMinPeek( nPartialElem, nActive ) ) {
-			nPartialElem = nItemWidth - nPartialElem;
+
+			var nPartialPeek = this.nPeek - this.settings.maxPeek;
+			nPartialElem = nItemWidth * ( nPartialPeek / 100 );
 			this.adjustIfAboveMaxPeek( nPartialElem, nActive );
+
+		}
+		this.settings.afterAdjust();
+	};
+
+	AdjustablePeek.prototype.clearInlineStyles = function() {
+
+		var defaultProperty = {
+			'width' : '',
+			'margin' : ''
+		};
+
+		$( this.element ).find( '.' + this.settings.itemClass ).css( defaultProperty );
+	};
+
+	AdjustablePeek.prototype.handleResize = function () {
+
+		if( this.settings.handleResize ) {
+			$( window ).resize( function () {
+				this.start();
+			}.bind( this ) );
+		}
+	};
+
+	AdjustablePeek.prototype.initPeekSettings = function ( ) {
+		var minPeek = this.settings.minPeek;
+		var maxPeek = this.settings.maxPeek;
+
+		if( ( minPeek > MAX_RANGE ) && ( minPeek < MIN_RANGE ) ) {
+			this.settings.minPeek = MIN_PEEK;
 		}
 
+		if( ( maxPeek > MAX_RANGE ) && ( maxPeek < MAX_RANGE ) ) {
+			this.settings.maxPeek = MAX_PEEK;
+		}
+
+		if( ( minPeek === maxPeek) ||
+				( minPeek > maxPeek ) ||
+				( maxPeek < minPeek ) ) {
+
+			this.settings.minPeek = MIN_PEEK;
+			this.settings.maxPeek = MAX_PEEK;
+
+		}
 	};
 
 	AdjustablePeek.prototype.adjustIfBelowMinPeek = function ( nPartialElem, nActive ) {
 
 		var bPeek = false;
 
-		if( this.nPeek < this.settings.minPeek ) {
+		if( ( this.nPeek > 0 ) && this.nPeek < this.settings.minPeek ) {
 			var properties = this.getCSSProps( this.getMargin( nPartialElem, nActive ) );
-			this.element.find( '.' + this.settings.itemClass ).css( properties );
+			$( this.element ).find( '.' + this.settings.itemClass ).css( properties );
 			bPeek =  true;
 		}
 		return bPeek;
@@ -47,16 +113,16 @@
 	AdjustablePeek.prototype.adjustIfAboveMaxPeek = function ( nPartialElem, nActive ) {
 
 		var bPeek = false;
-		if( this.nPeek > this.settings.maxPeek ) {
+		if( (this.nPeek > 0 ) && this.nPeek > this.settings.maxPeek ) {
 			var properties = this.getCSSProps( this.getMargin( nPartialElem, nActive ) );
-			this.element.find( '.' + this.settings.itemClass ).css( properties );
+			$( this.element ).find( '.' + this.settings.itemClass ).css( properties );
 			bPeek = true;
 		}
 		return bPeek;
 	};
 
 	AdjustablePeek.prototype.getMargin = function ( nPartialElem, nActiveBox ) {
-		var nMargin  = Math.ceil( nPartialElem / ( nActiveBox * 2 ) );
+		var nMargin  = nPartialElem / ( nActiveBox * 2 );
 		return ( nMargin ) ? nMargin : 0;
 	};
 
@@ -68,9 +134,8 @@
 		if( nMargin > maxMargin ) {
 			var nExtraWid = nMargin - maxMargin;
 			properties    = {	'margin' : '0 ' + maxMargin+ 'px',
-												'width' : this.itemElem.outerWidth() + nExtraWid };
+												'width' : this.itemElem.outerWidth() + ( nExtraWid * 2 ) + 'px' };
 		}
-
 		return properties;
 	};
 
@@ -90,10 +155,14 @@
 	};
 
 	$.fn.adjustablePeek.defaults = {
-		itemClass : 'item',
-		minPeek   : 30,
-		maxPeek   : 70,
-		maxMargin : 10,
+		//must be an ancestor or this element itself
+		containerId  : '',
+		itemClass    : 'item',
+		minPeek      : MIN_PEEK,
+		maxPeek      : MAX_PEEK,
+		maxMargin    : 10,
+		handleResize : true,
+		afterAdjust  : function() {}
 	};
 
 } )( jQuery );
